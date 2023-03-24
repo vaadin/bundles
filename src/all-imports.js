94cd27e402cfb320782c3d3f28ce5003377d5e6b
Zhe Sun <zhe@vaadin.com>
2023-03-24 12:10:49 +0200

    pick the ssr-dom-shim fixes

--- src/all-imports.js
+++ src/all-imports.js
@@ -104,6 +104,7 @@ import 'quickselect';
 // ignore bundle internal import 'lit-html';
 // ignore bundle internal import 'lit-element';
 // ignore bundle internal import '@lit/reactive-element';
+// ignore bundle internal import '@lit-labs/ssr-dom-shim';
 
 import 'lit';
 import 'lit/async-directive.js';
